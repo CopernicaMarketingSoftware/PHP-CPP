6c4ff800fcd7582b3bacb25ccd5e2c5374cf634f
Martijn Otto <martijn.otto@copernica.com>
2014-02-17 16:43:47 +0100

    Removed the static property from the example as this is not currently enabled

--- Examples/ConstStaticProp/cpp/mytestext.cpp
+++ Examples/ConstStaticProp/cpp/mytestext.cpp
@@ -1,7 +1,7 @@
 /**
  *  cppclassinphp.cpp
  *  @author Jasper van Eck<jasper.vaneck@copernica.com>
- * 
+ *
  *  An example file to show the working of using a C++ class in PHP.
  */
 
@@ -13,43 +13,41 @@ using namespace std;
 
 class MyTestExt : public Php::Base
 {
-    
+
 public:
     MyTestExt() {}
-    
+
     virtual ~MyTestExt() {}
 
     virtual void __construct() {}
-    
+
 };
 
 
 
 // Symbols are exported according to the "C" language
-extern "C" 
+extern "C"
 {
     // export the "get_module" function that will be called by the Zend engine
     PHPCPP_EXPORT void *get_module()
     {
         // create extension
         static Php::Extension extension("my_test_ext","0.1a");
-        
+
         // add the custom class ot the extension
         extension.add(
-            "MyTestClass", 
+            "MyTestClass",
             Php::Class<MyTestExt>({
-                
+
                 // Private PHP constructor! You can't instance object of MyTestClass
                 Php::Private("__construct", Php::Method<MyTestExt>(&MyTestExt::__construct)),
-                
+
                 Php::Const("version", "v0.01-alpha"),
                 Php::Const("PI", 3.14159265),
                 Php::Const("IMISNULL"),
-                
-                Php::Static("exp", 2.71828182846),
             })
         );
-                
+
         // return the extension module
         return extension.module();
     }
