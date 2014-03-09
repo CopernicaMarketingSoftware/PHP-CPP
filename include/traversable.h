bdca5b0454534413f10d13211885c086fafff35a
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2014-03-09 19:31:19 +0100

    made code in Value::mapValue pretier, updates to documentation, first setup for creating an iterator

---
+++ include/traversable.h
@@ -0,0 +1,29 @@
+/**
+ *  Traversable.h
+ *
+ *  Interface that should be implemented by classes that want to be
+ *  traversable. A traversable objects can be iterated over in a PHP
+ *  foreach loop.
+ *
+ *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
+ *  @copyright 2014 Copernica BV
+ */
+
+/**
+ *  Set up namespace
+ */
+namespace Php {
+
+/**
+ *  Class definition
+ */
+class Traversable
+{
+
+
+};
+
+/**
+ *  End namespace
+ */
+}
