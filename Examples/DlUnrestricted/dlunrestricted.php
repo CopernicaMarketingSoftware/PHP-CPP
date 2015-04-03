e1dc57a131fe297882a9d6dbf2d20fc7be662123
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2015-04-03 09:28:38 +0200

    added DlUnrestricted example extension, modified makefiles to use php-config to find out the extension-dir

---
+++ Examples/DlUnrestricted/dlunrestricted.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ *  functionvoid.php
+ *  @author Jasper van Eck<jasper.vaneck@copernica.com>
+ * 
+ *  An example file to show the working of a void function call.
+ */
+
+/*
+ *  Run the function with the given name. As you can see, the given name 
+ *  can be different from the actual function name.
+ */
+my_void_function();
