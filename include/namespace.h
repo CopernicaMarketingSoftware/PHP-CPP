572cd34158f1f91f769156519db10a8e737858e8
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-05-31 00:48:13 -0400

    Refactored add(const Interface&) method to clarify intent

--- include/namespace.h
+++ include/namespace.h
@@ -161,11 +161,12 @@ public:
      */
     Namespace &add(const Interface &interface)
     {
-        // skip when locked
-        if (locked()) return *this;
-
-        // make a copy and add it to the list of classes
-        _classes.push_back(std::unique_ptr<ClassBase>(new Interface(interface)));
+        // add only if unlocked
+        if (!locked())
+        {
+            // make a copy and add it to the list of classes
+            _classes.push_back(std::unique_ptr<ClassBase>(new Interface(interface)));
+        }
         
         // allow chaining
         return *this;
