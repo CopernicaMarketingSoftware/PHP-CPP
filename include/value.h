adff07c8fc97bf154903ba25bfbfab9df3daae6a
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-06-21 10:26:01 -0400

    Refactored operator-=(X&, Php::Value) to use C++ style cast to clarify intent

--- include/value.h
+++ include/value.h
@@ -1238,7 +1238,7 @@ PHPCPP_EXPORT std::ostream &operator<<(std::ostream &stream, const Value &value)
 template <typename X, typename std::enable_if<std::is_integral<X>::value>::type* = nullptr>
 X &operator+=(X &x, const Php::Value &value) { return x += static_cast<X>(value); }
 template <typename X, typename std::enable_if<std::is_integral<X>::value>::type* = nullptr>
-X &operator-=(X &x, const Php::Value &value) { return x -= (X)value; }
+X &operator-=(X &x, const Php::Value &value) { return x -= static_cast<X>(value); }
 template <typename X, typename std::enable_if<std::is_integral<X>::value>::type* = nullptr>
 X &operator*=(X &x, const Php::Value &value) { return x *= (X)value; }
 template <typename X, typename std::enable_if<std::is_integral<X>::value>::type* = nullptr>
