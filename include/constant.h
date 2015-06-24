4d21c60c95c8a21da0906d683cbd9098b1afb986
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-06-23 21:37:41 -0400

    Added API documentation for Constant(const char*, const char*)

--- include/constant.h
+++ include/constant.h
@@ -29,92 +29,99 @@ class ConstantImpl;
 class PHPCPP_EXPORT Constant
 {
 public:
-    /**
-     *  Constructor
-     *  @param  name            Constant name
-     *  @param  value           Constant value
-     *  @param  size            Size of the value (in case of a string)
-     */
-    Constant(const char *name, std::nullptr_t value = nullptr);
+	/**
+	 *  Constructor
+	 *  @param  name            Constant name
+	 *  @param  value           Constant value
+	 *  @param  size            Size of the value (in case of a string)
+	 */
+	Constant(const char *name, std::nullptr_t value = nullptr);
 
-    /**
-     * Constructor to create a constant for a boolean value
-     *
-     * @param name  Constant's name
-     * @param value Constant's value
-     */
-    Constant(const char *name, bool value);
+	/**
+	 * Constructor to create a constant for a boolean value
+	 *
+	 * @param name  Constant's name
+	 * @param value Constant's value
+	 */
+	Constant(const char *name, bool value);
 
-    /**
+	/**
 	 * Constructor to create a constant for a 32-bit integer
 	 *
 	 * @param name  Constant's name
 	 * @param value Constant's value
 	 */
-    Constant(const char *name, int32_t value);
+	Constant(const char *name, int32_t value);
 
-    /**
+	/**
 	 * Constructor to create a constant for a 64-bit integer
 	 *
 	 * @param name  Constant's name
 	 * @param value Constant's value
 	 */
-    Constant(const char *name, int64_t value);
+	Constant(const char *name, int64_t value);
 
-    /**
+	/**
 	 * Constructor to create a constant for a double precision
 	 * floating point number
 	 *
 	 * @param name  Constant's name
 	 * @param value Constant's value
 	 */
-    Constant(const char *name, double value);
-    Constant(const char *name, const char *value);
-    Constant(const char *name, const char *value, size_t size);
-    Constant(const char *name, const std::string &value);
+	Constant(const char *name, double value);
 
-    /**
-     *  Destructor
-     */
-    virtual ~Constant() {}
+	/**
+	 * Constructor to create a constant for a string literal
+	 *
+	 * @param name  Constant's name
+	 * @param value Constant's value
+	 */
+	Constant(const char *name, const char *value);
+	Constant(const char *name, const char *value, size_t size);
+	Constant(const char *name, const std::string &value);
 
-    /**
-     *  Add the constant to a class
-     *
-     *  You normally do not have to call this method yourself. You can simply
-     *  do one of the following method calls to create class constants:
-     *
-     *      myclass.property("MY_CONSTANT", "value", Php::Const);
-     *      myclass.constant("MY_CONSTANT", "value");
-     *      myclass.add(Php::Constant("MY_CONSTANT", "value"));
-     *
-     *  All of the calls have the same result, it is up to you to decide which
-     *  one suits you most. If you use the last one - using a Php::Constant
-     *  class - the PHP-CPP library will call this "addTo()" method internally
-     *  to forward the call to one of the other methods.
-     *
-     *  @param  clss        Class to which the constant is added
-     *  @internal
-     */
-    void addTo(ClassBase &clss) const;
+	/**
+	 *  Destructor
+	 */
+	virtual ~Constant() {}
+
+	/**
+	 *  Add the constant to a class
+	 *
+	 *  You normally do not have to call this method yourself. You can simply
+	 *  do one of the following method calls to create class constants:
+	 *
+	 *      myclass.property("MY_CONSTANT", "value", Php::Const);
+	 *      myclass.constant("MY_CONSTANT", "value");
+	 *      myclass.add(Php::Constant("MY_CONSTANT", "value"));
+	 *
+	 *  All of the calls have the same result, it is up to you to decide which
+	 *  one suits you most. If you use the last one - using a Php::Constant
+	 *  class - the PHP-CPP library will call this "addTo()" method internally
+	 *  to forward the call to one of the other methods.
+	 *
+	 *  @param  clss        Class to which the constant is added
+	 *  @internal
+	 */
+	void addTo(ClassBase &clss) const;
 
 private:
-    /**
-     *  Pointer to the actual implementation of the constant
-     *  @var    std::shared_ptr
-     */
-    std::shared_ptr<ConstantImpl> _impl;
+	/**
+	 *  Pointer to the actual implementation of the constant
+	 *  @var    std::shared_ptr
+	 */
+	std::shared_ptr<ConstantImpl> _impl;
 
-    /**
-     *  Get access to the implementation object
-     *  @return std::shared_ptr
-     */
-    const std::shared_ptr<ConstantImpl> &implementation() const { return _impl; }
+	/**
+	 *  Get access to the implementation object
+	 *  @return std::shared_ptr
+	 */
+	const std::shared_ptr<ConstantImpl> &implementation() const {return _impl;}
 
-    /**
-     *  The extension object has access to privates
-     */
-    friend class ExtensionImpl;
+	/**
+	 *  The extension object has access to privates
+	 */
+	friend class ExtensionImpl;
 
 };
 
