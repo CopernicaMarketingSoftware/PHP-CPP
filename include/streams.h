f06bd1a1e9c75aaf380048510d00fa167f08b63f
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-06-27 02:47:46 -0400

    Enhanced file documentation header

--- include/streams.h
+++ include/streams.h
@@ -1,13 +1,14 @@
 /**
- *  Streams.h
+ *  @file streams.h
  *
  *  Just like the standard std::cout and std::cerr objects to output data, you
  *  can use similar stream objects for outputting data to PHP. Php::out is the
  *  C++ equivalent of the PHP echo() function, and Php::err() is the C++ stream
  *  that behaves like calling trigger_error() from PHP.
  *
- *  Php::out << "this is example text" << std::endl;
- *  Php::err << "this is an error message" << std::endl;
+ *  Examples:
+ *      Php::out << "this is example text" << std::endl;
+ *      Php::err << "this is an error message" << std::endl;
  *
  *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
  *  @copyright 2014 Copernica BV
