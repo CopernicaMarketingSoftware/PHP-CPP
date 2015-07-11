3d6345b43cbcc06fb05ce4dc76277c88d378325e
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-07-11 12:25:14 -0400

    Refactored file structure and enhanced API documentation

--- include/argument.h
+++ include/argument.h
@@ -1,11 +1,12 @@
 /**
  *  @file argument.h
  *
- *  Class holds information about an argument that is passed to a function.
+ *  This file provides a class that holds information about an argument passed to a function.
+ *
  *  You'll need this class when you're defining your own functions.
  *
- *  The constructor of the argument is protected. Use the ByVal or ByRef
- *  classes instead.
+ *  The constructor of the class is protected so clients are directed to use the Php::ByVal or
+ *  Php::ByRef classes instead.
  *
  *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
  *  @copyright 2013, 2014 Copernica BV
@@ -23,163 +24,152 @@ class PHPCPP_EXPORT Argument
 {
 public:
     /**
-     *  Destructor
+     *  Default Destructor
      */
-    virtual ~Argument() = default;
+    virtual ~Argument() =default;
+
+    /**
+     *  Method stating whether this is a required argument or not
+     *
+     *  @return bool - true if the argument is required, false otherwise
+     *  @internal
+     */
+    bool required() const _NOEXCEPT
+    {
+        return m_required;
+    }
+
+    /**
+     *  Method to retrieve the name of the argument
+     *
+     *  @return const char * - C-String containing the name of the argument
+     */
+    const char *name() const _NOEXCEPT
+    {
+        return m_name;
+    }
+
+    /**
+     *  Method to retrieve the type of the argument
+     *
+     *  @return Php::Type - The type this argument is mapped to
+     */
+    Type type() const _NOEXCEPT
+    {
+        return m_type;
+    }
+
+    /**
+     *  Method to retrieve the class name if the type is a class
+     *
+     *  @return const char * - C-String containing the name of the class if the
+     *                         argument's type is that of a class
+     */
+    const char *classname() const _NOEXCEPT
+    {
+        return m_classname;
+    }
+
+    /**
+     *  Method stating whether it is allowed to pass parameter with a null value
+     *
+     *  @return bool - true if can be nullable, false otherwise
+     */
+    bool allowNull() const _NOEXCEPT
+    {
+        return m_nullable;
+    }
+
+    /**
+     *  Method stating whether this a parameter-by-reference
+     *
+     *  @return bool - true if its a reference, false otherwise
+     */
+    bool byReference() const _NOEXCEPT
+    {
+        return m_byReference;
+    }
 
 protected:
     /**
-     *  Constructor to create an argument by specifying the following
+     *  Constructor to create an object of this type by specifying the following
      *
-     *  @param name     Name of the argument
-     *  @param type     Argument type
-     *  @param required Is this argument required?
-     *  @param byref    Is this a reference argument
+     *  @param name     - Argument name
+     *  @param type     - Argument type
+     *  @param required - Is this argument required?
+     *  @param byref    - Is this a reference argument?
      */
     Argument(const char *name, Type type, bool required = true, bool byref = false)
-        : _name{name}
-        , _type{type}
-        , _classname{nullptr}
-        , _nullable{false}
-        , _required{required}
-        , _byReference{byref}
+        : m_name        { name     }
+        , m_type        { type     }
+        , m_classname   { nullptr  }
+        , m_nullable    { false    }
+        , m_required    { required }
+        , m_byReference { byref    }
     {}
 
     /**
-     *  Constructor to create an argument by specifying the following
+     *  Constructor to create an object of this type by specifying the following
      *
-     *  @param name      Name of the argument
-     *  @param classname Name of the class
-     *  @param nullable  Can it be null?
-     *  @param required  Is this argument required?
-     *  @param byref     Is this a reference argument?
+     *  @param name      - Argument name
+     *  @param classname - Class name
+     *  @param nullable  - Can it be null?
+     *  @param required  - Is this argument required?
+     *  @param byref     - Is this a reference argument?
      */
     Argument(const char *name, const char *classname, bool nullable = true,
              bool required = true, bool byref = false)
-        : _name{name}
-        , _type{Type::Object}
-        , _classname{classname}
-        , _nullable{nullable}
-        , _required{required}
-        , _byReference{byref}
+        : m_name       { name         }
+        , m_type       { Type::Object }
+        , m_classname  { classname    }
+        , m_nullable   { nullable     }
+        , m_required   { required     }
+        , m_byReference{ byref        }
     {}
-
-public:
-    /**
-     *  Is this a required argument?
-     *
-     *  @return bool true if the argument is required, false otherwise
-     *  @internal
-     */
-    bool required() const
-    {
-        return _required;
-    }
-
-    /**
-     *  Name of the argument
-     *
-     *  @return const char * C-String containing the name of the argument
-     */
-    const char *name() const
-    {
-        return _name;
-    }
-
-    /**
-     *  Type-hint for the argument
-     *
-     *  @return Type The type this argument is mapped to
-     */
-    Type type() const
-    {
-        return _type;
-    }
-
-    /**
-     *  If the type is a class, the name of the class
-     *
-     *  @return const char * C-String containing the name of the class if the
-     *                       argument's type is that of a class
-     */
-    const char *classname() const
-    {
-        return _classname;
-    }
-
-    /**
-     *  Is it allowed to pass parameter with a null value?
-     *
-     *  @return bool true if can be nullable, false otherwise
-     */
-    bool allowNull() const
-    {
-        return _nullable;
-    }
-
-    /**
-     *  Is this a parameter-by-reference?
-     *
-     *  @return bool true if its a reference, false otherwise
-     */
-    bool byReference() const
-    {
-        return _byReference;
-    }
 
 private:
     /**
-     *  Name of the argument
-     *
-     *  @var const char * C-String containing the name of the argument
+     *  Argument name
      */
-    const char *_name;
+    const char * m_name;
 
     /**
      *  Type of argument
-     *
-     *  @var Type The type this argument is mapped to
      */
-    Type _type;
+    Type m_type;
 
     /**
      *  The name of the class if this is a parameter that is supposed to be
      *  an instance of a class
-     *
-     *  @var const char * C-String containing the name of the class if the
-     *                    argument's type is that of a class
      */
-    const char *_classname;
+    const char * m_classname;
 
     /**
      *  May the parameter be null?
-     *
-     *  @var bool true if can be nullable, false otherwise
      */
-    bool _nullable;
+    bool m_nullable;
 
     /**
      *  Is this a required argument?
-     *
-     *  @var bool true if the argument is required, false otherwise
      */
-    bool _required;
+    bool m_required;
 
     /**
      *  Is this a 'by-reference' parameter?
-     *
-     *  @var bool true if its a reference, false otherwise
      */
-    bool _byReference;
+    bool m_byReference;
 };
 
 /**
- *  Old Visual C++ environments do not support initializer lists
+ *  Old Visual C++ environments do not support initializer lists,
+ *  therefore arguments should be passed as vectors
  */
-#if defined(_MSC_VER) && _MSC_VER < 1800
+#if defined(_MSC_VER) && (_MSC_VER < 1800)
 
 /**
- *  For old visual c++ compilers, arguments should be passed as vectors
+ *  A vector of arguments can be supplied to methods
+ *
+ *  @type Arguments - A vector of arguments
  */
 using Arguments = std::vector<Argument>;
 
@@ -191,7 +181,7 @@ using Arguments = std::vector<Argument>;
 /**
  *  A list of arguments can be supplied to methods
  *
- *  @type Arguments A list of arguments
+ *  @type Arguments - A list of arguments
  */
 using Arguments = std::initializer_list<Argument>;
 
