385dea06af3fbca98326461b93234905fb28bd1c
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-07-12 10:51:32 -0400

    Update array.h

--- include/array.h
+++ include/array.h
@@ -1,8 +1,8 @@
 /**
  *  @file array.h
  *
- *  Array is a value that defaults itself to model an array and upholds this
- *  invariant throughout its lifetime
+ *  This file provides a class that defaults itself to model an array
+ *  and upholds this invariant throughout its lifetime
  *
  *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
  *  @copyright 2013, 2014 Copernica BV
@@ -20,81 +20,92 @@ class PHPCPP_EXPORT Array : public Value
 {
 public:
     /**
-     *  Constructor that defaults itself to model an array
+     *  Constructor to create an object of this type that defaults itself
+     *  to model an array
      */
-    Array() : Value(Type::Array) {}
+    Array()
+        : Value{ Type::Array }
+    {}
 
     /**
-     *  Copy constructor from a value object
+     *  Copy constructor to create an object of this type
      *
      *  If value object's type is not that of an array an exception of type
-     *  FatalError is thrown
+     *  Php::FatalError is thrown
      *
-     *  @param value Must be a value that models an array
+     *  @param value - Must be a value that models an array
      */
-    Array(const Value &value) : Value(value)
+    Array(const Value &value)
+        : Value{ value }
     {
         // type must be valid
         if (value.type() != Type::Array) throw FatalError("Assigning a non-array to an array variable");
     }
 
     /**
-     *  Move constructor from a value object
+     *  Move constructor to create an object of this type
      *
      *  If value object's type is not that of an array an exception of type
-     *  FatalError is thrown
+     *  Php::FatalError is thrown
      *
-     *  @param value Must be a value that models an array
+     *  @param value - Must be a value that models an array
      */
-    Array(Value &&value)  _NOEXCEPT : Value(std::move(value))
+    Array(Value &&value) _NOEXCEPT
+        : Value{ std::move(value) }
     {
         // type must be valid
         if (value.type() != Type::Array) throw FatalError("Moving a non-array to an array variable");
     }
 
     /**
-     *  Constructor that creates an array from a std::vector of elements
+     *  Constructor to create an object of this type by specifying the following
      *
-     *  @param input A std::vector of elements
+     *  @param input - A std::vector of elements
      */
     template <typename T>
-    Array(const std::vector<T> &input) : Value(input) {}
+    Array(const std::vector<T> &input)
+        : Value{ input }
+    {}
 
     /**
      *  Constructor that creates an associative array from a std::map of
      *  key-value pairs
      *
-     *  @param input A std::map of key-value pairs
+     *  @param input - A std::map of key-value pairs
      */
     template <typename T>
-    Array(const std::map<std::string,T> &input) : Value(input) {}
+    Array(const std::map<std::string,T> &input)
+        : Value{ input }
+    {}
 
-// old visual c++ environments have no support for initializer lists
-#   if !defined(_MSC_VER) || _MSC_VER >= 1800
+// Old Visual C++ environments have no support for std::initializer_lists
+#   if !defined(_MSC_VER) || (_MSC_VER >= 1800)
 
     /**
      *  Constructor that creates an array from a std::initializer_list of
      *  Php::Value elements
      *
-     *  @param input A std::initializer_list of Php::Value elements
+     *  @param input - A std::initializer_list of Php::Value elements
      */
-    Array(const std::initializer_list<Value> &value) : Value(value) {}
+    Array(const std::initializer_list<Value> &value)
+        : Value{ value }
+    {}
 
-// end of visual c++ check
+// End of Visual C++ check
 #   endif
 
     /**
-     *  Destructor
+     *  Default destructor
      */
-    virtual ~Array() = default;
+    virtual ~Array() _NOEXCEPT =default;
 
     /**
      *  This method should not be called within the context of this type as its
      *  only here due to this type's dependency on its parent an only acts as a
      *  guard to uphold an invariant
      *
-     *  @param type Must be that of Type::Array else an exception of FatalError
-     *              is thrown
+     *  @param type - Must be that of Type::Array else an exception of Php::FatalError
+     *                is thrown
      */
     virtual Value &setType(Type type) override
     {
@@ -108,13 +119,13 @@ public:
     /**
      *  Assignment operator
      *
-     *  If the incoming Value object is not that of Type::Array an exception of
-     *  FatalError is thrown
+     *  If the incoming Php::Value object is not that of Type::Array an exception of
+     *  Php::FatalError is thrown
      *
-     *  @param  value  A Value object with a type of Type::Array
+     *  @param  value  - A Php::Value object with a type of Type::Array
      *
-     *  @return Array& This object containing new values corresponding to the
-     *                 input argument
+     *  @return Array& - This object containing new values corresponding to the
+     *                   input argument
      */
     Array &operator=(const Value &value)
     {
@@ -134,13 +145,13 @@ public:
     /**
      *  Move assignment operator
      *
-     *  If the incoming Value object is not that of Type::Array an exception of
+     *  If the incoming Php::Value object is not that of Type::Array an exception of
      *  FatalError is thrown
      *
-     *  @param  value  A Value object with a type of Type::Array
+     *  @param  value  - A Php::Value object with a type of Type::Array
      *
-     *  @return Array& This object containing new values corresponding to the
-     *                 input argument
+     *  @return Array& - This object containing new values corresponding to the
+     *                   input argument
      */
     Array &operator=(Value &&value) _NOEXCEPT
     {
