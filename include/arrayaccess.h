ceb35d5386e18ec36f773457acb9ec7a58d56b3b
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-07-12 10:54:16 -0400

    Update arrayaccess.h

--- include/arrayaccess.h
+++ include/arrayaccess.h
@@ -1,10 +1,10 @@
 /**
  *  @file arrayaccess.h
  *
- *  "Interface" that can be "implemented" by your class. If you do, you
- *  create your class like this:
+ *  This file provides an "Interface" that can be "implemented" by your class.
  *
- *  class MyClass : public Php::Base, public Php::ArrayAccess { ... }
+ *  If you do, you create your class as follows:
+ *     class MyClass : public Php::Base, public Php::ArrayAccess { ... }
  *
  *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
  *  @copyright 2014 Copernica BV
@@ -24,40 +24,40 @@ public:
     /**
      *  Check if an element exists by supplying its key to find it
      *
-     *  @param  key  Key to find the associated element
+     *  @param  key  - Key to find the associated element
      *
-     *  @return bool true if the element exist, false otherwise
+     *  @return bool - true if the element exist, false otherwise
      */
     virtual bool offsetExists(const Php::Value &key) = 0;
 
     /**
      *  Set a slot within the array with the specified key-value pair
      *
-     *  @param  key   Key to find the associated element
-     *  @param  value The element to associate with the key
+     *  @param key   - Key to find the associated value
+     *  @param value - The value to associate with the key
      */
     virtual void offsetSet(const Php::Value &key, const Php::Value &value) = 0;
 
     /**
-     *  Retrieve a copy of the element associated with the specified key
+     *  Retrieve a copy of the value associated with the specified key
      *
-     *  @param  key   Key to find the associated element
+     *  @param  key   - Key to find the associated value
      *
-     *  @return value The element to associate with the key
+     *  @return value - The value associated with the key
      */
     virtual Php::Value offsetGet(const Php::Value &key) = 0;
 
     /**
      *  Remove an element from the array associated with the specified key
      *
-     *  @param key Key to find the associated element
+     *  @param key - Key to find the associated element
      */
     virtual void offsetUnset(const Php::Value &key) = 0;
 
     /**
-     * Destructor
+     * Default destructor
      */
-    virtual ~ArrayAccess() = default;
+    virtual ~ArrayAccess() _NOEXCEPT =default;
 };
 
 /**
