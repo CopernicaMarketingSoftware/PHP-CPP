17cef742189b2d2071337816d4ba44f5ec377907
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-07-14 00:08:04 -0400

    Augmented the file's content
    Enhanced the file's API documentation, code structure, and added language keyword (noexcept) to methods for better machine generated code.

--- include/byval.h
+++ include/byval.h
@@ -1,10 +1,12 @@
 /**
- *  ByVal.h
+ *  @file byval.h
  *
- *  Overridden Argument class to specify by-value function arguments
+ *  This file provides a class that models the semantics of accepting an argument by value
  *
  *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
- *  @copyright 2013 Copernica BV
+ *  @author Rico Antonio Felix <ricoantoniofelix@yahoo.com>
+ *
+ *  @copyright 2013-2015 Copernica BV
  */
 
 /**
@@ -19,38 +21,50 @@ class PHPCPP_EXPORT ByVal : public Argument
 {
 public:
     /**
-     *  Constructor
-     *  @param  name        Name of the argument
-     *  @param  type        Argument type
-     *  @param  required    Is this argument required?
+     *  Constructor to create an object of this type by specifying the following
+     *
+     *  @param name     - What is the argument's name?
+     *  @param type     - What type of argument is this?
+     *  @param required - Is this argument required?
      */
-    ByVal(const char *name, Type type = Type::Null, bool required = true) : Argument(name, type, required, false) {}
+    ByVal(const char * name, Type type = Type::Null, bool required = true) _NOEXCEPT
+        : Argument{ name, type, required, false }
+    {}
 
     /**
-     *  Constructor
-     *  @param  name        Name of the argument
-     *  @param  classname   Name of the class
-     *  @param  nullable    Can it be null?
-     *  @param  required    Is this argument required?
+     *  Constructor to create an object of this type by specifying the following
+     *
+     *  @param name      - What is the argument's name?
+     *  @param classname - What is the name of the class this argument represents?
+     *  @param nullable  - Can it be null?
+     *  @param required  - Is this argument required?
      */
-    ByVal(const char *name, const char *classname, bool nullable = false, bool required = true) : Argument(name, classname, nullable, required, false) {}
+    ByVal(const char * name, const char * classname, bool nullable = false, bool required = true) _NOEXCEPT
+        : Argument{ name, classname, nullable, required, false }
+    {}
 
     /**
-     *  Copy constructor
-     *  @param  argument
+     *  Copy constructor to create an object of this type by specifying the following
+     *
+     *  @param argument - Where is the object of this type to copy from?
      */
-    ByVal(const ByVal &argument) : Argument(argument) {}
+    ByVal(const ByVal & argument) _NOEXCEPT
+        : Argument{ argument }
+    {}
 
     /**
-     *  Move constructor
-     *  @param  argument
+     *  Move constructor to create an object of this type by specifying the following
+     *
+     *  @param argument - Where is the object of this type to steal its possessions?
      */
-    ByVal(ByVal &&argument) _NOEXCEPT : Argument(argument) {}
+    ByVal(ByVal && argument) _NOEXCEPT
+        : Argument{ argument }
+    {}
 
     /**
-     *  Destructor
+     *  Default destructor
      */
-    virtual ~ByVal() {}
+    virtual ~ByVal() _NOEXCEPT =default;
 };
 
 /**
