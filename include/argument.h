4cce9f0e5c7296818892cccc5680bfedef5e3b31
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-07-14 00:03:11 -0400

    Augmented the file's content
    Enhanced the file's API documentation, code structure, added default copy and move constructors and added language keyword (noexcept) to methods for better machine generated code.

--- include/argument.h
+++ include/argument.h
@@ -1,14 +1,17 @@
 /**
  *  @file argument.h
  *
- *  Class holds information about an argument that is passed to a function.
+ *  This file provides a class that holds information about an argument passed to a function.
+ *
  *  You'll need this class when you're defining your own functions.
  *
- *  The constructor of the argument is protected. Use the ByVal or ByRef
- *  classes instead.
+ *  The constructors of the class are protected so clients are directed to use the related
+ *  Php::ByVal or Php::ByRef classes instead.
  *
  *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
- *  @copyright 2013, 2014 Copernica BV
+ *  @author Rico Antonio Felix <ricoantoniofelix@yahoo.com>
+ *
+ *  @copyright 2013-2015 Copernica BV
  */
 
 /**
@@ -23,152 +26,184 @@ class PHPCPP_EXPORT Argument
 {
 public:
     /**
-     *  Destructor
+     *  Default destructor
      */
-    virtual ~Argument() = default;
+    virtual ~Argument() _NOEXCEPT =default;
+
+    /**
+     *  Method stating whether this is a required argument or not
+     *
+     *  @return bool - true if the argument is required, false otherwise
+     *  @internal
+     */
+    bool required() const _NOEXCEPT
+    {
+        return m_required;
+    }
+
+    /**
+     *  Method to retrieve the name of the argument
+     *
+     *  @return const char * - C-String containing the name of the argument
+     */
+    const char * name() const _NOEXCEPT
+    {
+        return m_name;
+    }
+
+    /**
+     *  Method to retrieve the type of the argument
+     *
+     *  @return Php::Type - The type this argument is mapped to
+     */
+    Type type() const _NOEXCEPT
+    {
+        return m_type;
+    }
+
+    /**
+     *  Method to retrieve the class name if the type is a class
+     *
+     *  @return const char * - C-String containing the name of the class if the
+     *                         argument's type is that of a class
+     */
+    const char * classname() const _NOEXCEPT
+    {
+        return m_classname;
+    }
+
+    /**
+     *  Method stating whether it is allowed to pass the argument with a null value
+     *
+     *  @return bool - true if can be null, false otherwise
+     */
+    bool allowNull() const _NOEXCEPT
+    {
+        return m_nullable;
+    }
+
+    /**
+     *  Method stating whether this a by-reference argument
+     *
+     *  @return bool - true if its a reference, false otherwise
+     */
+    bool byReference() const _NOEXCEPT
+    {
+        return m_byReference;
+    }
 
 protected:
     /**
-     *  Constructor
-     *  @param  name        Name of the argument
-     *  @param  type        Argument type
-     *  @param  required    Is this argument required?
-     *  @param  byref       Is this a reference argument
+     *  Constructor to create an object of this type by specifying the following
+     *
+     *  @param name     - What is the argument's name?
+     *  @param type     - What type of argument is this?
+     *  @param required - Is this argument required?
+     *  @param byref    - Is this a reference argument?
      */
-    Argument(const char *name, Type type, bool required = true, bool byref = false) :
-        _name(name), _type(type), _required(required), _byReference(byref) {}
+    Argument(const char * name, Type type, bool required = true, bool byref = false) _NOEXCEPT
+        : m_name        { name     }
+        , m_type        { type     }
+        , m_classname   { nullptr  }
+        , m_nullable    { false    }
+        , m_required    { required }
+        , m_byReference { byref    }
+    {}
 
     /**
-     *  Constructor
-     *  @param  name        Name of the argument
-     *  @param  classname   Name of the class
-     *  @param  nullable    Can it be null?
-     *  @param  required    Is this argument required?
-     *  @param  byref       Is this a reference argument?
+     *  Constructor to create an object of this type by specifying the following
+     *
+     *  @param name      - What is the argument's name?
+     *  @param classname - What is the name of the class this argument represents?
+     *  @param nullable  - Can it be null?
+     *  @param required  - Is this argument required?
+     *  @param byref     - Is this a reference argument?
      */
-    Argument(const char *name, const char *classname, bool nullable = true, bool required = true, bool byref = false) :
-        _name(name), _type(Type::Object), _classname(classname), _nullable(nullable), _required(required), _byReference(byref) {}
-
-public:
-    /**
-     *  Is this a required argument?
-     *  @return bool
-     *  @internal
-     */
-    bool required() const
-    {
-        return _required;
-    }
+    Argument(const char * name, const char * classname, bool nullable = true, bool required = true, bool byref = false) _NOEXCEPT
+        : m_name        { name         }
+        , m_type        { Type::Object }
+        , m_classname   { classname    }
+        , m_nullable    { nullable     }
+        , m_required    { required     }
+        , m_byReference { byref        }
+    {}
 
     /**
-     *  Name of the argument
-     *  @return const char *
+     *  Copy constructor to create an object of this type by specifying the following
+     *
+     *  @param argument - Where is the object of this type to copy from?
      */
-    const char *name() const
-    {
-        return _name;
-    }
+     Argument(const Argument & argument) _NOEXCEPT =default;
 
-    /**
-     *  Type-hint for the argument
-     *  @return Type
+     /**
+     *  Move constructor to create an object of this type by specifying the following
+     *
+     *  @param argument - Where is the object of this type to steal its possessions?
      */
-    Type type() const
-    {
-        return _type;
-    }
-
-    /**
-     *  If the type is a class, the name of the class
-     *  @return const char *
-     */
-    const char *classname() const
-    {
-        return _classname;
-    }
-
-    /**
-     *  Is it allowed to pass parameter with a null value?
-     *  @return bool
-     */
-    bool allowNull() const
-    {
-        return _nullable;
-    }
-
-    /**
-     *  Is this a parameter-by-reference?
-     *  @return bool
-     */
-    bool byReference() const
-    {
-        return _byReference;
-    }
+     Argument(Argument && argument) _NOEXCEPT =default;
 
 private:
     /**
-     *  Name of the argument
-     *  @var const char *
+     *  The argument's name
      */
-    const char *_name = nullptr;
+    const char * m_name;
 
     /**
-     *  Type of argument
-     *  @var Type
+     *  The type of argument
      */
-    Type _type = Type::Null;
+    Type m_type;
 
     /**
-     *  Classname, if this is a parameter that is supposed to be an instance of a class
-     *  @var std::string
+     *  The name of the class if this is an argument that is supposed to be
+     *  an instance of a class
      */
-    const char *_classname = nullptr;
+    const char * m_classname;
 
     /**
-     *  May the parameter be null?
-     *  @var bool
+     *  The answer to whether the argument can be null
      */
-    bool _nullable = false;
+    bool m_nullable;
 
     /**
-     *  Is this a required argument
-     *  @var    bool
+     *  The answer to whether this argument is required
      */
-    bool _required = true;
+    bool m_required;
 
     /**
-     *  Is this a 'by-reference' parameter?
-     *  @var    bool
+     *  The answer to whether this is a 'by-reference' argument
      */
-    bool _byReference = false;
+    bool m_byReference;
 };
 
 /**
- *  Old Visual C++ environments do not support initializer lists
+ *  Old Visual C++ environments do not support std::initializer_list,
+ *  therefore arguments should be passed as a std::vector
  */
-#if defined(_MSC_VER) && _MSC_VER < 1800
+#if defined(_MSC_VER) && (_MSC_VER < 1800)
 
 /**
- *  For old visual c++ compilers, arguments should be passed as vectors
+ *  A vector of arguments can be supplied to methods
+ *
+ *  @type Arguments - A vector of arguments
  */
 using Arguments = std::vector<Argument>;
 
 /**
- *  Other compilers, and visual C++ 2013 do support initializer lists
+ *  Other compilers, and Visual C++ 2013 do support std::initializer_list
  */
 #else
 
 /**
  *  A list of arguments can be supplied to methods
- *  @type   Arguments
+ *
+ *  @type Arguments - A list of arguments
  */
 using Arguments = std::initializer_list<Argument>;
 
 /**
- *  End of visual C++ check
+ *  End of Visual C++ check
  */
 #endif
-
 
 /**
  *  End of namespace
