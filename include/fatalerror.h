47f394135b2f1b3cef6505ac560bdea826f2108e
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-08-13 00:55:14 -0400

    Refactored Php::FatalError::report() to use noexcept keyword to generate better machine code...

--- include/fatalerror.h
+++ include/fatalerror.h
@@ -59,10 +59,11 @@ public:
     }
 
     /**
-     *  Report this error as a fatal error
-     *  @return bool
+     *  Method stating whether this is a fatal error
+     *
+     *  @return bool - true if its a fatal error, false otherwise
      */
-    virtual bool report() const override;
+    virtual bool report() const _NOEXCEPT override;
 };
 
 /**
