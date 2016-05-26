2c669069c58a6f855a7701b416740f8bdbd25bbc
Martijn Otto <martijn.otto@copernica.com>
2016-05-26 09:49:23 +0200

    Fix uninitialized data when initializing ini entries

--- zend/ini.cpp
+++ zend/ini.cpp
@@ -21,6 +21,7 @@ void Ini::fill(zend_ini_entry_def *ini_entry, int module_number)
 {
     ini_entry->modifiable       = static_cast<int>(_place);
     ini_entry->name             = _name.data();
+    ini_entry->name_length      = _name.size();
     ini_entry->on_modify        = OnUpdateString;
     ini_entry->mh_arg1          = nullptr;
     #ifdef ZTS
@@ -30,6 +31,7 @@ void Ini::fill(zend_ini_entry_def *ini_entry, int module_number)
     #endif
     ini_entry->mh_arg3          = nullptr;
     ini_entry->value            = _value.data();
+    ini_entry->value_length     = _value.size();
     ini_entry->displayer        = nullptr;
 }
 
