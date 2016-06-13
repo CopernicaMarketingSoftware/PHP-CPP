b8a4b5fb2db6ef9860c4205cf90401e67b458f62
Martijn Otto <martijn.otto@copernica.com>
2016-06-13 16:19:41 +0200

    Made the output streams thread_local so they work correctly in a multithreaded environment. Added compatibility layer for the ever-failing Microsoft compilers. Fixes issue #90

---
+++ include/thread_local.h
@@ -0,0 +1,18 @@
+/**
+ *  thread_local.h
+ *
+ *  Some compilers claim to be C++11 compliant, yet fail to
+ *  implement basic features, such as the thread_local storage
+ *  specifier. These little cripples we help out a bit by
+ *  redefining the keyword to whatever proprietary cruft
+ *  they are using.
+ *
+ *  @copyright 2016 Copernica B.V.
+ */
+
+// are we dealing with an outdated sort of compiler?
+#ifdef _MSC_VER && _MSC_VER < 1500
+    // can't use thread_local, but we can use their
+    // limited storage scope specifier in this case
+    #define thread_local __declspec( thread )
+#endif
